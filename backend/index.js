a156a64ea48790121324a5a0aeccd121c649a539
Prem Shakya <146803260+prem9084@users.noreply.github.com>
2025-05-02 22:44:54 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(morgan("dev"));
 app.use(
   cors({
-    origin: ["https://task-handlers.netlify.app", "http://localhost:5173/"],
+    origin: ["https://task-handlers.netlify.app/", "http://localhost:5173/"],
   })
 );
 
