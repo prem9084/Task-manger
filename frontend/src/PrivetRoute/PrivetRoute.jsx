febd7e91d72d8c5758f0f6ad2cfb524a7dfabd33
Prem Shakya <146803260+prem9084@users.noreply.github.com>
2025-05-02 23:07:16 +0530

    Update PrivetRoute.jsx

--- frontend/src/PrivetRoute/PrivetRoute.jsx
+++ frontend/src/PrivetRoute/PrivetRoute.jsx
@@ -4,7 +4,7 @@ import { Outlet } from "react-router-dom";
 import Login from "../pages/Login";
 import axios from "axios";
 
-// const API = "https://task-manager-mern-2i7g.onrender.com";
+const API = "https://task-manger-ykjf.onrender.com";
 
 export default function PrivetRoute() {
   const [ok, setOk] = useState(false);
@@ -12,7 +12,7 @@ export default function PrivetRoute() {
 
   useEffect(() => {
     const authCheck = async () => {
-      const res = await axios.get("/api/user/user-auth");
+      const res = await axios.get(`${API}/api/user/user-auth`);
       if (res.data.ok) {
         setOk(true);
       } else {
